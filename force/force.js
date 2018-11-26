00b3a63b0001640cb234f2a5dac1026555a834a6
ThuyNT13 <ngo.thuy38@gmail.com>
2018-11-25 21:17:44 -0800

    Add forceCollide to prevent overlap

--- force/force.js
+++ force/force.js
@@ -1,5 +1,9 @@
-const width = 300, height = 300;
-let nodes = [{}, {}, {}, {}, {}]
+const width = 400, height = 400;
+const numNodes = 100;
+
+const nodes = d3.range(numNodes).map(function (d) {
+  return { radius: Math.random() * 25 }
+})
 
 // const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
 
@@ -9,8 +13,12 @@ const svg = d3.select("body").append("svg")
 
 // call forceSimulation(), passing array of objects
 const simulation = d3.forceSimulation(nodes) 
-  .force("charge", d3.forceManyBody())
+  // attracting or repelling force: + attract; - repel; default -30
+  .force("charge", d3.forceManyBody().strength(5))
+  //  setting the center of the gravity force
   .force("center", d3.forceCenter(width / 2, height / 2))
+  .force("collision", d3.forceCollide().radius(function(d) { 
+    return d.radius; } ))
   .on("tick", ticked);
 
 //  joins the nodes array to circle elements and updates their positions:
@@ -20,8 +28,8 @@ function ticked() {
   
     circles.enter()
       .append("circle")
-      .attr("fill", "cadetblue")
-      .attr("r", 5)
+      .attr("fill", "orange")
+      .attr("r", function(d) { return d.radius; })
       .merge(circles)
       .attr("cx", function(d) { return d.x; })
       .attr("cy", function(d) { return d.y; })
