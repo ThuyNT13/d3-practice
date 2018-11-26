148a460d3e72634a746c27de253c6c5da0e9ba1e
ThuyNT13 <ngo.thuy38@gmail.com>
2018-11-25 22:01:01 -0800

    Add forceY

--- force/force.js
+++ force/force.js
@@ -1,23 +1,32 @@
-const width = 700, height = 400;
-const numNodes = 100;
-const colorScale = ["orange", "lightblue", "#B19CD9"];
-const xCenter = [100, 300, 500];
+// https://bl.ocks.org/d3indepth/fee5ce57c3fc3e94c3332577d1415df4
+
+const width = 700, height = 200;
+const numNodes = 40;
+// const xCenter = [100, 300, 500];
+
+// const colorScale = ["orange", "lightblue", "#B19CD9"];
+
+const xScale = d3.scaleLinear()
+  .domain([0,1])
+  .range([0,width]);
 
 const nodes = d3.range(numNodes).map(function (d, i) {
   return { 
     radius: Math.random() * 25,
-    category: i % 3
+    // category: i % 3,
+    color: "orange",
+    value: Math.random()
   }
 })
 
-// const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+// const colorScale = d3.scaleOrdinal(d3.schemeCategory10); 
 
 const svg = d3.select("body").append("svg")
   .attr("width", width)
   .attr("height", height)
   .append("g")
-  // move group down 200, right 50
-  .attr("transform", "translate(50, 200)");
+  // move group down 100, right 50
+  .attr("transform", "translate(50, 100)");
 
 // call forceSimulation(), passing array of objects
 const simulation = d3.forceSimulation(nodes) 
@@ -26,8 +35,13 @@ const simulation = d3.forceSimulation(nodes)
   //  setting the center of the gravity force
   // towards an x-coordinate
   .force("x", d3.forceX().x(function(d) {
-    return xCenter[d.category];
+    // return xCenter[d.category];
+    return xScale(d.value);
   }))
+  .force("y", d3.forceY().y(function(d) {
+    return 0;
+  }))
+  // deter elements overlapping 
   .force("collision", d3.forceCollide().radius(function(d) { 
     return d.radius; } ))
   .on("tick", ticked);
@@ -39,7 +53,8 @@ function ticked() {
   
     circles.enter()
       .append("circle")
-      .attr("fill", function(d) { return colorScale[d.category]; })
+      // .attr("fill", function(d) { return colorScale[d.category]; })
+      .attr("fill", function(d) { return d.color; })
       .attr("r", function(d) { return d.radius; })
       .merge(circles)
       .attr("cx", function(d) { return d.x; })
